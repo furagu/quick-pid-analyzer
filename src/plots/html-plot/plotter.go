e35d1ec9ad7175c60aa296911fe24baaef9ffd1d
Yurii Maltsev <yurii.maltsev@xing.com>
2019-05-24 10:54:55 +0200

    handle async errors from plotting charts

--- src/plots/html-plot/plotter.go
+++ src/plots/html-plot/plotter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gobuffalo/packr"
 	"github.com/pkg/errors"
 	"io"
+	"strings"
 	"sync"
 	"text/template"
 )
@@ -26,9 +27,10 @@ type PlotConfig struct {
 // HtmlPlotter implements plots.HtmlPlotter interface, and plots multiple SVG plots to
 // a single HTML file.
 type HtmlPlotter struct {
-	cfg    PlotConfig
-	charts []plots.Chart
-	wg     sync.WaitGroup
+	cfg       PlotConfig
+	charts    []plots.Chart
+	wg        sync.WaitGroup
+	latestErr error
 }
 
 // NewHtmlPlotter crates a new plotter instance.
@@ -48,12 +50,17 @@ func (p *HtmlPlotter) Plot(target io.Writer) error {
 	var charts []io.Reader
 
 	wg := &sync.WaitGroup{}
+	errs := &errList{}
 	for _, chart := range p.charts {
 		buf := &bytes.Buffer{}
 		charts = append(charts, buf)
-		p.plotChartAsync(chart, buf, wg)
+		p.plotChartAsync(chart, buf, errs, wg)
 	}
 	wg.Wait()
+
+	if !errs.empty() {
+		return errs
+	}
 
 	cols := p.cfg.Cols
 	if cols == 0 {
@@ -71,13 +78,37 @@ func (p *HtmlPlotter) plotChart(chart plots.Chart, target io.Writer) error {
 	return errors.Wrapf(err, "could not draw chart '%s' on the plot", chart.Name())
 }
 
-func (p *HtmlPlotter) plotChartAsync(chart plots.Chart, target io.Writer, wg *sync.WaitGroup) {
+func (p *HtmlPlotter) plotChartAsync(chart plots.Chart, target io.Writer, errs *errList, wg *sync.WaitGroup) {
 	wg.Add(1)
 	go func() {
-		err := p.plotChart(chart, target)
-		if err != nil {
-			panic(err)
+		defer wg.Done()
+
+		if err := p.plotChart(chart, target); err != nil {
+			errs.add(err)
 		}
-		wg.Done()
 	}()
+}
+
+type errList struct {
+	list []error
+	mu   sync.Mutex
+}
+
+func (l *errList) add(err error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.list = append(l.list, err)
+}
+
+func (l *errList) empty() bool {
+	return len(l.list) == 0
+}
+
+func (l errList) Error() string {
+	var messages []string
+	for _, err := range l.list {
+		messages = append(messages, err.Error())
+	}
+	return strings.Join(messages, "\n")
 }
